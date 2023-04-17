d2abf278d588d74fdbdde8d2ddea3c59420cbbd3
hail zanbar <hailzanbar1@gmail.com>
2023-04-17 15:02:37 +0300

    check

---
+++ client/src/pages/HomePage_BACKUP_1111.jsx
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+
+export default function HomePage() {
+    return(
+        <div>
+<<<<<<< HEAD
+            <label htmlFor="home">Home hadas working on</label>
+=======
+            <label htmlFor="home">Home h</label>
+>>>>>>> 9f79150 (check)
+        </div>
+    );
+}
