c3ba007fb0926b9c57f9e7b0f8d49babffa7d2c7
hadaseshel <hadseshel@gmail.com>
2023-05-01 20:19:03 +0300

    add section of market status in the home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -74,19 +74,10 @@ export default function HomePage() {
                     </div>
                 </div>
             </div>
-            <div
-            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-            aria-hidden="true"
-            >
-            <div
-                className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-                style={{
-                clipPath:
-                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                }}
-            />
-            </div>
-            <div className="mx-auto max-w-2xl py-8 sm:py-26 lg:py-30">
+
+       
+
+            <div className="mx-auto max-w-2xl py-16 sm:py-32 lg:py-38">
                 <div className="text-center">
                     <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                     Let's talk with numbers
@@ -128,7 +119,7 @@ export default function HomePage() {
                                             </h2>
                                             <h5 class="card-title">Occupations</h5>
                                             <p class="card-text">
-                                                The number of occupations in the system.
+                                                The number of occupations in the system
                                             </p>
                                         </div>
                                     </div>
@@ -150,6 +141,68 @@ export default function HomePage() {
                 }}
             />
             </div>
+
+
+            <div className="mx-auto max-w-2xl py-16 sm:py-32 lg:py-38">
+                <div className="text-center">
+                    <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                    Career market status
+                    </h1>
+                    
+                        <div class="container text-center mt-8">
+                            <div class="row mb-4">
+                                <div class="col">
+                                    <div class="card h-100"> 
+                                        <div class="card-body">
+                                            <h2 class="card-title countNumber">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-pie-chart-fill" viewBox="0 0 16 16">
+                                                    <path d="M15.985 8.5H8.207l-5.5 5.5a8 8 0 0 0 13.277-5.5zM2 13.292A8 8 0 0 1 7.5.015v7.778l-5.5 5.5zM8.5.015V7.5h7.485A8.001 8.001 0 0 0 8.5.015z"/>
+                                                </svg>
+                                            </h2>
+                                            <h5 class="card-title">Unemployed</h5>
+                                                <p class="card-text">
+                                                    The percentage of unemployed residents in Israel is 3.9
+                                                </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card h-100"> 
+                                        <div class="card-body">
+                                            <h2 class="card-title countNumber">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-wallet-fill" viewBox="0 0 16 16">
+                                                <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v2h6a.5.5 0 0 1 .5.5c0 .253.08.644.306.958.207.288.557.542 1.194.542.637 0 .987-.254 1.194-.542.226-.314.306-.705.306-.958a.5.5 0 0 1 .5-.5h6v-2A1.5 1.5 0 0 0 14.5 2h-13z"/>
+                                                <path d="M16 6.5h-5.551a2.678 2.678 0 0 1-.443 1.042C9.613 8.088 8.963 8.5 8 8.5c-.963 0-1.613-.412-2.006-.958A2.679 2.679 0 0 1 5.551 6.5H0v6A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-6z"/>
+                                            </svg>
+                                                
+                                            </h2>
+                                            <h5 class="card-title">Israeli employees</h5>
+                                            <p class="card-text">
+                                                The number of employed Israelis is 4.316 in millions
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card h-100"> 
+                                        <div class="card-body">
+                                            <h2 class="card-title countNumber">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-door-open-fill" viewBox="0 0 16 16">
+                                                    <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
+                                                </svg>    
+                                            </h2>
+                                            <h5 class="card-title">Vacancies</h5>
+                                            <p class="card-text">
+                                                The number of vacancies in the Israeli market is 126,804
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
         </div>
+        
         );
 }
