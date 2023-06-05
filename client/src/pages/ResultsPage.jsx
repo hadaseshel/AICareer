0f1d5721a7a484ecde6f2709d747d4ab4e2838b7
hail zanbar <hailzanbar1@gmail.com>
2023-06-05 11:40:23 +0300

    Edit requests in result page

--- client/src/pages/ResultsPage.jsx
+++ client/src/pages/ResultsPage.jsx
@@ -11,15 +11,19 @@ export default function ResultsPage() {
 
         // try to get the recommendation from DB if exist.
         try {
-            const response = await axios.get("/api/result", {
+            const {data} = await axios.get("/api/recommend/result", {
                 params: { user_id: user._id },
             });
-            const user_results = response.data.results;
-            setResults(user_results)
-            if (!user_results) {
+            if (data) {
+                console.log(data);
+                const user_results = data.results;
+                setResults(user_results);
+            }
+            else {
                 console.log("IN GET REC");
                 const {data} = await axios.get('/api/recommend', {params: {user_id: user._id, user_answers: user_answers}});
-                await axios.post('/api/result', {user_id: user._id, user_answers: data.recommendations});
+                console.log(data)
+                await axios.post('/api/recommend/result', {user_id: user._id, results: data.recommendations});
                 setResults(data.recommendations);
             }
         } catch (e) {
@@ -45,11 +49,27 @@ export default function ResultsPage() {
             }
             getAnswers();
         }   
-      }, [ready, user]);
+    }, [ready, user]);
 
-      if (!ready || !user) {
+    if (!ready || !user) {
         return 'Loading...';
-      }
+    }
+
+    if (results.length < 1) {
+        return (
+            <div className="d-flex flex-column align-items-center justify-content-center mt-40">
+                <div className="row mb-10">
+                    <div className="spinner-border text-success" style={{width: 150, height: 150}} role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                </div>
+                <div className="row mt-10">
+                    <h1 style={{ fontSize: "2rem" }}>We process your answers...</h1>
+                </div>
+            </div>
+       );
+    }
+
     
     return (
         <div className="text-center max-w-lg mx-auto mt-10">
