c2bb2b8dc70b2d6668d98ea5af5352a0dc81a10c
hadaseshel <hadseshel@gmail.com>
2023-06-16 13:44:35 +0300

    add cors of http://localhost, add tests section

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost', 'http://localhost:3000'],
   }));
 
 mongoose.connect(process.env.MONGO_URL);
--- server/tests/test_server.py
+++ server/tests/test_server.py
@@ -1,25 +1,80 @@
 import unittest
 from unittest import TestCase
-from unittest.mock import patch
-from app import app
+import requests
+import unittest
 
 
-class TestOccupationsRoute(TestCase):
+class TestServer(TestCase):
     def setUp(self):
-        self.client = app.test_client()
+        self.url = "http://localhost:4000/api"
+               
+    #################################### Tests for Occupation function ####################################
+    def test_get_occupations_count(self):
+        url_occupations_count = "/occupations/count"
+        full_url = self.url + url_occupations_count
+        
+        response = requests.get(full_url) # Send a GET request to the server
+        data = response.json()  # Extract the JSON data from the response
+        
+        self.assertEqual(response.status_code, 200) # Assert if the response status code is not 200
+        num_of_occupations = 858 # Assert if the response status code is not 200
+        self.assertEqual(data, num_of_occupations)
+        
+    def test_get_occupations_by_description(self):
+        description = "Animal Breeders"
+        occupations = "/occupations/"
+        url = f"{self.url + occupations}?Description={description}"
+        
+        response = requests.get(url) # Send a GET request to the server
+        data = response.json()  # Extract the JSON data from the response
+        
+        self.assertEqual(response.status_code, 200) # Assert if the response status code is not 200
+        self.assertEqual(data["Description"], description)
 
-    @patch('app.Occupation.count_documents')
-    def test_get_occupations_count(self, mock_count_documents):
-        mock_count_documents.return_value = 10
-
-        response = self.client.get('/api/occupations/count')
-        data = response.get_json()
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(data, 10)
-
-    def tearDown(self):
-        pass
+    def test_get_occupations_by_code(self):
+        code = "45-2021.00"
+        occupations = "/occupations"
+        url = f"{self.url +occupations}/Code/?Code={code}"
+        
+        response = requests.get(url) # Send a GET request to the server
+        data = response.json() # Extract the JSON data from the response
+        
+        self.assertEqual(response.status_code, 200) # Assert if the response status code is not 200
+        self.assertEqual(data["Code"], code)
+    
+    #######################################################################################################
+    
+    ####################################### Tests for Home function #######################################
+    
+    # 2 functions
+    
+    #######################################################################################################
+    
+    ##################################### Tests for Questions function ####################################
+    
+    # 6 functions
+    
+    #######################################################################################################
+    
+    ###################################### Tests for Response function ####################################
+    
+    # 2 functions
+    
+    #######################################################################################################
+    
+    ###################################### Tests for Recommend function ###################################
+    
+    # 3 functions
+    
+    #######################################################################################################
+    
+    ######################################### Tests for User function #####################################
+    
+    # 6 functions
+    
+    #######################################################################################################
 
 
 if __name__ == '__main__':
     unittest.main()
+    
